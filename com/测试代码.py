258333b35f7df7dea72e46f5d2c334463b940c58
小仙的朱雀 <962139864@qq.com>
2018-09-06 09:07:38 +0800

    asd

--- com/测试代码.py
+++ com/测试代码.py
@@ -9,24 +9,23 @@ from PIL import Image
 import pytesseract
 
 # 上面都是导包，只需要下面这一行就能实现图片文字识别
-text = pytesseract.image_to_data(Image.open(r'E:\work\python\main\android\1.png'), lang='chi_sim')
+text = pytesseract.image_to_string(Image.open(r'E:\work\python\com\android\1.png'), lang='chi_sim')
 print(text)
 print(type(text))
-a = text.split("	")
-print("-----------")
-for i in range(0, len(a)):
-    # print(a[i])
-    if "密" in a[i]:
-        print(a[i - 5])
-        print(a[i - 4])
-        print(a[i - 3])
-        print(a[i - 2])
-        break
-print("-----------")
-# print(a)
-# for text1  in a:
-#     print(text1)
-#     print("-------------------")
-aaa = "1"
-if aaa == "1":
-    print("====")
+if text.__contains__("召 唤"):
+    print("true")
+# a = text.split("	")
+# print("-----------")
+# for i in range(0, len(a)):
+#     # print(a[i])
+#     if "密" in a[i]:
+#         print(a[i - 5])
+#         print(a[i - 4])
+#         print(a[i - 3])
+#         print(a[i - 2])
+#         break
+# print("-----------")
+#
+# aaa = "1"
+# if aaa == "1":
+#     print("====")
